d029daa5b1da579e5749595e9ac3cd8eb1783ae7
Sebastian Pardo <s-pardo@192.168.0.6>
2022-11-21 19:13:42 -0500

    Create Correct Arrays

---
+++ index.js
@@ -0,0 +1,6 @@
+const subject = ["You", "U", "Ye", "Yous", "Youse"];
+
+const adverb = ["absolutly", "truly", "definitely", "doubtlessly", "unquestionably"]; 
+
+const adjective = ["a sapaperra", "a pirobo", "a gonococo", "a carechimba", "a cacorro"]; 
+
